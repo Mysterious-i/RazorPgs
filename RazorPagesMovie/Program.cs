93a664f18bc3fb3ba53441a70430ad868005dbc2
Neev Tighnavard <neev.t@Neevs.local>
2024-03-18 14:39:33 -0400

    init commit

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RazorPagesMovie;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages(); //adds services for Razor Pages to the app.
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();//
+
+        app.MapRazorPages();//adds endpoints for Razor Pages to the IEndpointRouteBuilder.
+
+        app.Run();
+    }
+}
+
